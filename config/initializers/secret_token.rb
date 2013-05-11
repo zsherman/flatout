8852969581aca15a3ec64daeed475434c8c718fc
Zach Sherman <zksherm@gmail.com>
2013-05-11 19:15:09 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flatout::Application.config.secret_token = '87732130b9cc2656d6b052576be273e2fceda6923c19c76f9728fd1482890699aa5d73a4a182674759e351f76833ca3d1b40852f13e0891e62cf46367f7ecc98'
