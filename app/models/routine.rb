58a7c7b41e334975eef3b918ce9da16b8cbada67
Zach Sherman <zksherm@gmail.com>
2013-05-16 17:57:37 +0200

    added some models

---
+++ app/models/routine.rb
@@ -0,0 +1,3 @@
+class Routine < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511200846) do
+ActiveRecord::Schema.define(:version => 20130516154621) do
+
+  create_table "exercises", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "routines", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
