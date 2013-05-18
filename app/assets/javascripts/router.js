22a2b8b7aa9a6d53d488bf2716165abea8235298
Zach Sherman <zksherm@gmail.com>
2013-05-18 20:32:12 +0200

    basic workout and exercise layouts

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,7 @@
 Flatout.Router.map(function(match){
   this.route("dashboard", { path: "/" });
   this.route("builder", { path: "/builder" });
+  this.route("exercises", { path: "/exercises" });
+  this.route("workouts", { path: "/workouts" });
 })
 
