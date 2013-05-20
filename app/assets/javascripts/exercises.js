6bb767965fadffedc73b137ae9fc287f97d51320
Zach Sherman <zksherm@gmail.com>
2013-05-20 17:21:34 +0200

    addings some start page functionality

---
+++ app/assets/javascripts/exercises.js
@@ -0,0 +1,24 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+
+$(function() {
+
+	$exercises = $('.routine li');
+
+	$('.routine a.next').click(function(e) {
+		e.preventDefault();
+		$active = $('li.active');
+		$active.removeClass('active');
+		$active.next('li').addClass('active');
+	})
+
+	// $('.routine a.restart').click(function(e) {
+	// 	e.preventDefault();
+	// 	$(this).removeClass('active');
+	// 	$('.routine li').first().addClass('active');
+	// })
+
+
+});
